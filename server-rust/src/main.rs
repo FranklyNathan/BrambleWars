02d326bae438e6d8e01f1383a58d05e5a7014a57
jpfrazz <jpfrazz@proton.me>
2025-07-31 22:26:41 -0400

    changed server echo message to protobuf def

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -1,4 +1,10 @@
-use axum::{extract::{ws::{Message, WebSocket}, WebSocketUpgrade}, response::Response, routing::any, Router};
+use prost::Message;
+use axum::extract::{ws::{self, WebSocket}, WebSocketUpgrade};
+use axum::{response::Response, routing::any, Router};
+
+pub mod bramble {
+    include!(concat!(env!("OUT_DIR"), "/bramble.rs"));
+}
 
 async fn handler(ws: WebSocketUpgrade) -> Response {
     dbg!("handling connection");
@@ -14,14 +20,25 @@ async fn handle_socket(mut socket: WebSocket) {
         };
 
         match msg {
-            Message::Text(ref msg) => println!("{}", msg),
-            Message::Close(ref msg) => println!("Socket Closed: {:?}", msg),
+           ws::Message::Text(ref msg) => {
+               println!("{}", msg);
+
+               let msg = bramble::EchoMessage {
+                   message: msg.to_string(),
+               };
+               
+               let mut message_buf = Vec::new();
+               msg.encode(&mut message_buf).unwrap();
+
+               if socket.send(ws::Message::binary(message_buf)).await.is_err() {
+                   return;
+               }
+           }
+            ws::Message::Close(ref msg) => println!("Socket Closed: {:?}", msg),
             _ => println!("non text msg recieved"),
         }
 
-        if socket.send(msg).await.is_err() {
-            return;
-        }
+
     }
 }
 
