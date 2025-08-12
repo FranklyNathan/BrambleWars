d161b49efb518de66543d9f834cdc9ab5d51460a
jpfrazz <jpfrazz@gmail.com>
2025-08-11 20:35:29 -0400

    added Auction and ServerState structs

--- server-rust/src/lib.rs
+++ server-rust/src/lib.rs
@@ -1,3 +1,9 @@
+use std::{collections::HashMap, time::Duration};
+use axum::extract::ws::WebSocket;
+use tokio::time::Instant;
+
+use crate::bramble::AuctionState;
+
 pub mod message_handler;
 pub mod websocket_handler;
 pub mod auction_handler;
@@ -6,6 +12,20 @@ pub mod bramble {
     include!(concat!(env!("OUT_DIR"), "/bramble.rs"));
 }
 
-pub struct ServerState {
-    
+pub struct Lot {
+    items: Box<[u32]>,
+    highest_bid: u32,
+}
+
+pub struct Auction<'a> {
+    host: &'a WebSocket,
+    current_lot: Lot,
+    remaining_lots: Vec<Lot>,
+    start_time: Instant,
+    current_countdown: Duration,
+    state: AuctionState,
+}
+
+pub struct ServerState<'a> {
+    auctions: HashMap<u32, Auction<'a>>,
 }
